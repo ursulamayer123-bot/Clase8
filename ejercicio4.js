c2520bd96952b20510eb8c11fd07e6978183a2ae
Ursula Mayer <ursulamayer123@gmail.com>
2025-12-05 17:14:27 -0300

    Ejercicios clase 8

--- ejercicio4.js
+++
@@ -1,7 +0,0 @@
-const prompt = require ('prompt-sync')();
-let numero = parseInt (prompt('Ingrese un número entero:'));
-if (numero %2 === 0){
-    console.log ("El número es par.");
-} else {
-    console.log("El número es impar.")
-}
--- ejercicio5.js
+++
@@ -1,3 +0,0 @@
-const prompt = require ('prompt-sync')();
-let nombre = prompt("Ingrese su nombre: ");
-console.log( "Bienvenida al club " + nombre + ".")
